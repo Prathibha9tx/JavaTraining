e656d52e27d16afa355c13abe2092ac91a4e6a52
Prathibha249m <prathibha249m@gmail.com>
2026-09-19 20:27:30 +0530

    constructor overload example

---
+++ Overloading/src/com/oops/overload/ConstructorOverload.java
@@ -0,0 +1,15 @@
+package com.oops.overload;
+
+public class ConstructorOverload {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("Prathibha",9,"CS");
+		e1.getDetails();
+		Employee e2 = new Employee("Uthsavi",3,"EC");
+		e2.getDetails();
+		Employee e3 = new Employee("Raj",10,"MECH");
+		e3.getDetails();
+		
+	}
+
+}
